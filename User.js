5fc55feec90ddbfc69b49374181b9466b1a0dca9
anuradha fernando <anufdo@gmail.com>
2023-02-26 05:21:44 +0530

    project setup

--- User.js
+++ User.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   age: { type: Number, min: 0, max: 100 },
   status: { type: String, enum: ["active", "inactive", "pending"] },
 });
+
+userSchema.methods.sayHi = function () {
+  console.log("Hi");
+};
 
 module.exports = mongoose.model("User", userSchema);
--- index.js
+++ index.js
@@ -9,10 +9,40 @@ mongoose.connect("mongodb://127.0.0.1/m7", {
   useUnifiedTopology: true,
 });
 
+const userData = [
+  {
+    firstName: "Saman",
+    lastName: "Silva",
+    age: 30,
+    email: "samansliva@gmail.com",
+    status: "active",
+  },
+  {
+    firstName: "Nimal",
+    lastName: "Fernando",
+    age: 35,
+    email: "nimalfernando@gmail.com",
+    status: "active",
+  },
+  {
+    firstName: "Kamal",
+    lastName: "Silva",
+    age: 40,
+    email: "kamalsilva@gmail.com",
+    status: "pending",
+  },
+];
+
+// async function insertManyUsers() {
+//   await User.insertMany(userData);
+// }
+
+// insertManyUsers().then(() => console.log("Inserted"));
+
 // list all users
 async function listAll() {
   const users = await User.find();
   console.log(users);
 }
 
-listAll();
+// listAll();
